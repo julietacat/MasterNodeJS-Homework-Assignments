7ad02c8ceb9cb68729fba445d6a81c100eae9439
julietacat <45194708+julietacat@users.noreply.github.com>
2018-12-18 08:37:04 +0300

    Add files via upload

---
+++ Homework assignment -2/lib/handlers.js
@@ -0,0 +1,75 @@
+/*
+*
+* Handlers
+*
+*/
+
+// Dependencies
+
+var _users = require('./handlers/users');
+var _tokens = require('./handlers/tokens');
+var _menu = require('./handlers/menu');
+var _orders = require('./handlers/orders');
+var _checkout = require('./handlers/checkout');
+
+// Container
+var handlers = {};
+
+// Handlers
+
+// ==> NOT FOUND
+handlers.notFound = (data, callback) => {
+  callback(404, { data });
+};
+
+
+// ==> USERS
+handlers.users = (data, callback) => {
+  var acceptableMethods = ['post', 'get', 'put', 'delete'];
+  if (acceptableMethods.indexOf(data.method) > -1) {
+    _users[data.method](data, callback);
+  } else {
+    callback(405);
+  }
+};
+
+// Token 
+handlers.tokens = (data, callback) => {
+  var acceptableMethods = ['post', 'get', 'put', 'delete'];
+  if (acceptableMethods.indexOf(data.method) > -1) {
+    _tokens[data.method](data, callback);
+  } else {
+    callback(405);
+  }
+};
+
+handlers.menu = (data, callback) => {
+  var acceptableMethods = ['get'];
+  if (acceptableMethods.indexOf(data.method) > -1) {
+    _menu[data.method](data, callback);
+  } else {
+    callback(405);
+  }  
+};
+
+handlers.orders = (data, callback) => {
+  var acceptableMethods = ['get','post','put','delete'];
+  if (acceptableMethods.indexOf(data.method) > -1) {
+    _orders[data.method](data, callback);
+  } else {
+    callback(405);
+  }
+};
+
+handlers.checkout = (data, callback) => {
+  var acceptableMethods = ['post','get','put','delete'];
+  if (acceptableMethods.indexOf(data.method) > -1 ) {
+    _checkout[data.method](data,callback);
+  } else {
+    callback(405);
+  }
+}
+
+
+// Exports
+module.exports = handlers;
